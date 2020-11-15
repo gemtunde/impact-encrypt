dcb6bd62ea38c68d9faea6478fe789743b9f4be6
gemtunde <52404803+gemtunde@users.noreply.github.com>
2020-11-15 17:36:33 +0100

    Add files via upload

---
+++ precache-manifest.e7e93a2ade2bb83ba65687b03dc5d5f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41865be95ecf9942401a7bc7c35bb1f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2fc89ce8b9dde4371e7",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b2fc89ce8b9dde4371e7",
+    "url": "/static/js/2.ae76c9d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae76c9d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889224fd2f20ae1b9b19",
+    "url": "/static/js/main.68a5fe51.chunk.js"
+  },
+  {
+    "revision": "9c6fb2a44e38d7c61afa",
+    "url": "/static/js/runtime-main.3f300b2a.js"
+  },
+  {
+    "revision": "59aba11f42909667344ec1c32d91302e",
+    "url": "/static/media/img_1.59aba11f.png"
+  },
+  {
+    "revision": "0703c4eec363a46a6128af7983909166",
+    "url": "/static/media/img_2.0703c4ee.png"
+  },
+  {
+    "revision": "21b30ea1f51efbf5a66e785e8e8d350f",
+    "url": "/static/media/img_4.21b30ea1.png"
+  },
+  {
+    "revision": "34cd38df78b621bc67cfb655ec37ce6c",
+    "url": "/static/media/img_6.34cd38df.png"
+  }
+]);
